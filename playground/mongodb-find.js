94d34144936c8c389fcd0f61d75acaef900c4225
Joshua Rippon <joshua@pomerolpartners.com>
2018-02-01 16:27:34 +0000

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,38 @@
+const { MongoClient, ObjectId } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) return console.log('Unable to connect to MongoDB server');
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectId("5a7335ba28265a3d9c4a2b7e")
+    // })
+    // .toArray()
+    // .then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find({
+    // })
+    // .count()
+    // .then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({
+        name: "Andrew"
+    })
+    .toArray()
+    .then((docs) => {
+        console.log('Todos');
+        console.log(JSON.stringify(docs));
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });
+    // db.close();
+});
