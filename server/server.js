37bbf9111057b261f2f013ae3b94765bac513c90
Joshua Rippon <joshua@pomerolpartners.com>
2018-02-02 10:01:08 +0000

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    console.log(req.body);
     var todo = new Todo({
         text: req.body.text
     });
@@ -22,9 +21,13 @@ app.post('/todos', (req, res) => {
     });
 })
 
-// app.get('/todos', (req, res) => {
-
-// })
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e)
+    })
+})
 
 // app.get('/todos/:id', (req, res) => {
 
@@ -34,4 +37,4 @@ app.listen(3000, () => {
     console.log("Started on port 3000");
 });
 
-
+module.exports = { app };
