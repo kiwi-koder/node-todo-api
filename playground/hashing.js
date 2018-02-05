8058d3164b45897c6159103a61682921c8ed5eea
Joshua Rippon <joshua@pomerolpartners.com>
2018-02-05 11:33:16 +0000

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,29 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
-var decoded = jwt.verify(token, '123abcd');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$3639/.T/Psb67BIvkkTDHONd7iyANqW8Ad6rA2yt4OybBPsYBpPUa'
+
+bcrypt.compare('123abc!', hashedPassword, (err, res) => {
+    console.log(res);
+})
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abcd');
+// console.log('decoded', decoded);
 
 // var message = 'I am user number 4';
 
